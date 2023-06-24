bfa6119d25c9b0c82acc1a71354aa66720557d59
jonathans199 <jso1919@gmail.com>
2023-06-24 10:15:57 -0400

    added git basics lesson

---
+++ 06-git-basics.js
@@ -0,0 +1,20 @@
+/***
+ *
+ * Frequent Git commands
+ * - git add 'filename' => will add 1 file to staging area
+ * - git add . => will add all modified files to staging area
+ * - git commit -m 'message here' => is how we label the box/stagin area
+ * - git push => is how we push our changes to github
+ *
+ * Check status
+ * - git status
+ */
+
+/**
+ * set up and push origin
+ * - git push -u origin main => this to setup upstream, so that you could use git push (use only once)
+ * - git push origin main => otherwise you will have to use this one all the time
+ * 
+ * - git push => is to push the changes
+ *
+ */
